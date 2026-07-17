3d527cd219ec4219a071795bf6f7bc92717a68ec
RahulGaikwad2327 <rahulgaikwad2327@gmail.com>
2026-07-17 14:12:29 +0530

    Time: 3 ms (60.35%) | Memory: 58.8 MB (43.73%) - LeetSync

---
+++ 57-insert-interval/insert-interval.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+
+    let ans = [];
+
+    let i=0;
+
+    while(i < intervals.length && intervals[i][1] < newInterval[0]){
+        ans.push(intervals[i]);
+        i++;
+    }
+    
+    while(i < intervals.length && intervals[i][0] <= newInterval[1]){
+
+        newInterval[0] = Math.min(intervals[i][0] , newInterval[0]);
+        newInterval[1] = Math.max(intervals[i][1] , newInterval[1]);
+        i++;
+        }
+
+         ans.push(newInterval);
+
+        while(i < intervals.length){
+            ans.push(intervals[i]);
+            i++;
+        }
+       return ans;
+
+};
