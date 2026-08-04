49513b2a80c19db4022f5453c43c9beb7a72ed73
RahulGaikwad2327 <rahulgaikwad2327@gmail.com>
2026-08-04 10:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (96.38%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,44 +3,88 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
+  let n = nums.length;
+  let low = 0;
+  let mid = 0;
+  let high = n-1;
 
-   let zero = 0;
-   let one = 0;
-   let two = 0;
+  while( mid <= high){
 
-   for(let i=0; i<nums.length; i++){
+    if(nums[mid] === 0){
+        let temp  = nums[low];
+        nums[low] = nums[mid];
+        nums[mid] = temp;
+        low++;
+        mid++;
+    }
 
-   if(nums[i] === 0){
-    zero++;
-   }
-   else if(nums[i] === 1){
-    one++;
-   }
-   else { 
-     two++;
-   }
-   }
+   else if (nums[mid] == 1) {
+       mid++;
+}
+    else {
+     let temp  = nums[mid];
+        nums[mid] = nums[high];
+        nums[high] = temp;
+        high--;
+}
+  }
 
-   let i = 0;
+  return nums;
 
-   while(zero > 0){ 
-    nums[i] = 0;
-    zero--;
-    i++;
-   }
 
-   while(one > 0){
-    nums[i] = 1;
-    one--;
-    i++;
-   }
 
-   while(two > 0){
-    nums[i] = 2;
-    two--;
-    i++;
-   }
 
-   return nums;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+//BRUTE FORCE SOLUTION
+//    let zero = 0;
+//    let one = 0;
+//    let two = 0;
+
+//    for(let i=0; i<nums.length; i++){
+
+//    if(nums[i] === 0){
+//     zero++;
+//    }
+//    else if(nums[i] === 1){
+//     one++;
+//    }
+//    else { 
+//      two++;
+//    }
+//    }
+
+//    let i = 0;
+
+//    while(zero > 0){ 
+//     nums[i] = 0;
+//     zero--;
+//     i++;
+//    }
+
+//    while(one > 0){
+//     nums[i] = 1;
+//     one--;
+//     i++;
+//    }
+
+//    while(two > 0){
+//     nums[i] = 2;
+//     two--;
+//     i++;
+//    }
+
+//    return nums;
     
-};
+// };
