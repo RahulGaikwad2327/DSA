2fb3393d86bb834bd3129695241ea0ae8420f2ed
RahulGaikwad2327 <rahulgaikwad2327@gmail.com>
2026-08-14 14:24:47 +0530

    Time: 41 ms (34.32%) | Memory: 59.4 MB (47.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,28 +4,27 @@
  */
 var lengthOfLongestSubstring = function(s) {
    
-    let left = 0;
-    let right = 0;
-    let max = 0;
-    let map = new Map();
+   let map = new Map();
+   let left = 0;
+   let right = 0;
+   let max = 0;
 
-       while(right < s.length) {
+   while(right < s.length){
+    let char = s[right];
 
-        let char = s[right];
+    if(map.has(char)){
+        left = Math.max(left , map.get(char) + 1);
 
-        if (map.has(char)) {
-            left = Math.max(left, map.get(char) + 1);
-        } 
-            map.set(char, right);
-        
-
-       
-
-        let length = right - left + 1;
-        max = Math.max(max, length);
-        right++;
     }
 
+    map.set(char, right);
+    
+    let length = right-left+1;
+
+    max = Math.max(length, max);
+
+    right++;
+   }
     return max;
 };
     
