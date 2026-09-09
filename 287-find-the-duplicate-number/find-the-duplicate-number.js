e138ae61e78bb1afd21ab085369f0927ef53f949
RahulGaikwad2327 <rahulgaikwad2327@gmail.com>
2026-09-09 10:19:05 +0530

    Time: 4 ms (95.23%) | Memory: 63.1 MB (83.55%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    
+    
+    
+    let slow = nums[0];
+    let fast = nums[0];
+
+    while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+        if(fast === slow){
+             break;
+        }
+    }
+    slow = nums[0];
+
+    while(slow !== fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+
+};
