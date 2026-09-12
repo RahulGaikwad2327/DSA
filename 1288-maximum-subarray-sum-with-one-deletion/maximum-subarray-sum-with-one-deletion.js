09614b715cc482f7b432f70cbfbf57a2b5c5d9c1
RahulGaikwad2327 <rahulgaikwad2327@gmail.com>
2026-09-12 16:19:51 +0530

    Time: 4 ms (55.42%) | Memory: 58.1 MB (39.76%) - LeetSync

---
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumSum = function(arr) {
+
+    let nodelete = arr[0];
+    let onedelete  = -Infinity;
+    let answer = arr[0];
+
+    for(let i=1; i<arr.length; i++){
+        let prevnodelete = nodelete;
+
+        nodelete = Math.max(nodelete + arr[i], arr[i]);
+        onedelete = Math.max(onedelete + arr[i], prevnodelete);
+        answer = Math.max(answer, nodelete , onedelete);
+    }
+
+    return answer;
+    
+};
